9042a42c5ef20f0723d2c50a87c06e315ff6408a
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-18 19:06:55 +0300

    fix(docs): theme prop

--- src/docs/Responsive.tsx
+++ src/docs/Responsive.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box } from '../components/Box'
-import { TextBox } from '../components/Text'
+import { Text } from '../components/Text'
 import { useBreakpointIndex, useMediaMatch } from '../hooks'
 import { useResponsiveValue } from '../hooks/useResponsiveValue'
 
@@ -15,11 +15,11 @@ export const UseBreakpointIndexDemo: React.FC = () => {
 
   return (
     <Box width={1} height="200px" p="l" bg="accentDark">
-      <TextBox p="s">Breakpoint index: {bp}</TextBox>
-      <TextBox p="s" variant="body2">
+      <Text p="s">Breakpoint index: {bp}</Text>
+      <Text p="s" variant="body2">
         Media matches map: {JSON.stringify(matches, null, 2)}
-      </TextBox>
-      <TextBox p="s">Responsive value: {responsiveValue} </TextBox>
+      </Text>
+      <Text p="s">Responsive value: {responsiveValue} </Text>
     </Box>
   )
 }
