7733f0c6ffbee1358ec1cd34c7319efe7ef7389a
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-20 19:31:31 +0300

    feat(shadows): add

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -4,4 +4,5 @@ export const shadows = {
   medium: '0 4px 12px rgba(0, 0, 0, 0.15)',
   high: '0px 10px 40px rgba(0, 0, 0, 0.2)',
   box: '0 1px 0 0 #ddd6cf',
+  book: '0 0 100px rgb(0 0 0 / 30%)',
 }
